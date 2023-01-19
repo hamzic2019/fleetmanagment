bb248993e96918029db3b7c8ab7afca4e76fa157
hamzic2019 <hamzicharis96@gmail.com>
2023-01-20 00:46:54 +0100

    added state to change active category

--- client/src/pages/Invoices/Invoices.jsx
+++ client/src/pages/Invoices/Invoices.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import {useState} from 'react'
 import InvoiceListItem from '../../components/InvoiceListItem/InvoiceListItem'
 import './Invoices.css'
 
 const Invoices = () => {
+  const [active, setActive] = useState('All Invoices')
+
+  const handelActive = (name) => setActive(name);
+  
   return (
     <div className='invovicesWrapper'>
         <div className="invoices-wrapper-upper">
@@ -18,10 +22,22 @@ const Invoices = () => {
         <div className="invoices-list">
           <div className="invoices-list-options">
                 <div className="invoices-list-options-left">
-                    <a href="#" className='active'>All Invoices</a>
-                    <a href="#">Paid</a>
-                    <a href="#">Unpaid</a>
-                    <a href="#">Archived</a>
+                    <a 
+                      onClick={() => handelActive('All Invoices')} 
+                      className={active === 'All Invoices' ? 'active': ''}
+                    >All Invoices</a>
+                    <a 
+                      onClick={() => handelActive('Paid')} 
+                      className={active === 'Paid' ? 'active': ''}
+                    >Paid</a>
+                    <a 
+                      onClick={() => handelActive('Unpaid')} 
+                      className={active === 'Unpaid' ? 'active': ''}
+                    >Unpaid</a>
+                    <a 
+                      onClick={() => handelActive('Archived')} 
+                      className={active === 'Archived' ? 'active': ''}
+                    >Archived</a>
                     
                 </div>
                 <div className="invoices-list-options-right">
